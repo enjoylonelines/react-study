78f7d5bcce3c902622e9e2139127264859cf09ea
enjoylonelines <hobeom5022@naver.com>
2024-02-22 14:53:30 +0900

    docs: pages 라우팅

--- react-apple-app/src/App.jsx
+++ react-apple-app/src/App.jsx
@@ -1,30 +1,33 @@
-import { styled } from 'styled-components'
+import { Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Nav from './components/Nav.jsx'
-import Banner from './components/Banner.jsx'
-import Row from './components/Row.jsx'
-import requests from './API/request.js'
-function App() {
-
+import Nav from './components/Nav'
+import MainPage from './pages/MainPage'
+import SearchPage from './pages/SearchPage'
+import LoginPage from './pages/LoginPage'
+import DetailPage from './pages/DetailPage'
+const Layout = () => {
   return (
-    <div className='app'>
-      <Container>
-        <Nav />
-        <Banner/>
-        <Row title='Trending Now' id='TN' fetchUrl={requests.fetchTrending} />
-        <Row title='Top Rated' id='TR' fetchUrl={requests.fetchTopRated} />
-        <Row title='Action Movies' id='AM' fetchUrl={requests.fetchActionMovies} />
-        <Row title='Comedy Movies' id='CM' fetchUrl={requests.fetchComedyMovies} />
-      </Container>
+    <div>
+      <Nav />
+
+      <Outlet />
     </div>
   )
 }
 
-const Container = styled.main`
-  position: relative;
-  display: block;
-  top: 70px;
-  padding: 0 calc(3.5vw + 5px);
-`
+function App() {
+  return (
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<Layout />} >
+          <Route index element={<LoginPage />} />
+          <Route path='main' element={<MainPage />} />
+          <Route path=':movieId' element={<DetailPage />} />
+          <Route path='search' element={<SearchPage />} />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
 
 export default App
