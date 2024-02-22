852527f89956d2f37ede92c0a57d77af24394f22
enjoylonelines <hobeom5022@naver.com>
2024-02-22 19:02:02 +0900

    feat: 검색 페이지 구현

--- react-apple-app/src/components/Nav.jsx
+++ react-apple-app/src/components/Nav.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components'
 
 const Nav = () => {
+  const navigate = useNavigate();
+  const [searchValue, setSearchValue] = useState('');
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  }
+
   return (      
     <NavWrapper>
       <Logo>
@@ -11,10 +19,46 @@ const Nav = () => {
           onClick={() => (window.location.href = '/')}
         /> 
       </Logo>
+
+      <Input 
+        value={searchValue}
+        onChange={handleChange}
+        className='nav__input'
+        type='text'
+        placeholder='영화를 검색해주세요.'
+      />
+    
+      <Login>Login</Login>
+    
     </NavWrapper>
   )
 }
 
+const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: rgba(0,0,0,0.582);
+  border-radius: 5px;
+  color: white;
+  padding: 5px;
+  border: 1px solid lightgray;
+`
+const Login = styled.a`
+  background-color: rgba(0,0,0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`
 const NavWrapper = styled.nav`
   position: fixed;
   top: 0;
@@ -22,7 +66,7 @@ const NavWrapper = styled.nav`
   right: 0;
   height: 70px;
   background-color: #000000;
-  dixplay: flex;
+  display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 36px;
@@ -34,11 +78,12 @@ const Logo = styled.a`
   width: 70px;
   font-size: 0;
   display: inline-block;
-  margin-top: 10px;
+  margin-bottom: 5px;
 
   img {
     display: block;
     width: 100%;
   }
 `
+
 export default Nav
--- react-apple-app/src/pages/SearchPage/index.jsx
+++ react-apple-app/src/pages/SearchPage/index.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "../../API/axios";
+import { useLocation } from "react-router-dom"
 
 const SearchPage = () => {
+  const [searchResults, setSearchResults] = useState([]);
+  const useQuery = () => new URLSearchParams(useLocation().search);
+  let query = useQuery();
+  const searchTerm = query.get('q');
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const response = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      setSearchResults(response.data.results);
+      console.log(searchResults)
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <div>
-      
+
     </div>
   )
 }
