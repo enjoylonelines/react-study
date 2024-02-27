285757dae88c7b9dcd919d206c5abf493e9f42ce
enjoylonelines <hobeom5022@naver.com>
2024-02-27 20:28:55 +0900

    에러 핸들링

--- react-context-app/client/src/Components/ErrorBanner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ErrorBanner = ({ message }) => {
-  const errorMessage = message || '에러입니다.';
-  return <div style={{backgroundColor: 'red'}}>{errorMessage}</div>
-  
-}
-
-export default ErrorBanner
