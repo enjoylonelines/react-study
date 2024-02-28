ce767eec4d63a337f022d360b818bc8674aa58a7
enjoylonelines <hobeom5022@naver.com>
2024-02-28 17:05:42 +0900

    useSelector & useDispatch

--- react-redux-app/src/App.tsx
+++ react-redux-app/src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './reducers';
 
 type Props = {
   value: any;
@@ -8,6 +10,9 @@ type Props = {
 }
 
 function App({ value, onIncrement, onDecrement }: Props) {
+  const dispatch = useDispatch();
+  const counter = useSelector((state: RootState) => state.counter);
+  const todos: string[] = useSelector((state: RootState) => state.todos);
   const [todoValue, setTodoValue] = useState('');
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -15,15 +20,18 @@ function App({ value, onIncrement, onDecrement }: Props) {
   }
   function addTodo(e: React.ChangeEvent<HTMLFormElement>) {
     e.preventDefault();
+    dispatch({ type: 'ADD_TODO', text: todoValue });
     setTodoValue('');
   }
-  
+
   return (
     <div className="App">
-      {/* Clicked: {value} time */}
+      Clicked: {counter} time
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
-
+      <ol>
+        {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+      </ol>
       <form onSubmit={addTodo} >
         <input type='text' value={todoValue} onChange={handleChange} />
         <input type="submit" />
--- react-redux-app/src/index.tsx
+++ react-redux-app/src/index.tsx
@@ -35,3 +35,4 @@ store.subscribe(render);
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
